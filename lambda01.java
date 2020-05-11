8eaf47fb6ae57d32839ba76a7c9887c41c443d60
steve wates <swates@mobifed.com>
2020-05-11 16:02:30 +0100

    lambda01

---
+++ lambda01.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList; 
+
+public class lambda01 {
+    public static void main(String[]args) {
+        // Creating an ArrayList with elements 
+        // {1, 2, 3, 4} 
+        ArrayList<Integer> intList = new ArrayList<Integer>(); 
+        intList.add(1); 
+        intList.add(2); 
+        intList.add(3); 
+        intList.add(4); 
+
+        System.out.println("Print using Java7 for loop:");
+        // Java7 for loop 
+        for (Integer item : intList) {
+            System.out.format("\nValue = %d", item);
+        }
+
+        System.out.println("Print using Java8 lambda:");
+        // Java 8 lambda
+        intList.forEach(i -> System.out.format("\nValue = %d", i));
+    }
+}
